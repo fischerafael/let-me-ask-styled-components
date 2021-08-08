09081782baeb4753e59c9b0b09e97d44abe7d2cf
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-08 13:17:16 -0300

    very basic style

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -2,7 +2,7 @@ import { handleNavigateTo } from '../utils/handleNavigateTo'
 
 export const PageNewRoom = () => {
     return (
-        <div className="w-screen h-screen flex items-center justify-center">
+        <div className="w-screen h-screen flex flex-col items-center justify-center">
             <input type="text" placeholder="Nome da Sala" />
             <button>Criar Sala</button>
             <a onClick={() => handleNavigateTo('/')}>
