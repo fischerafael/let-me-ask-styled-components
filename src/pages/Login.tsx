7fa7191e4d13049dcc80d0a060669e332b3c2037
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-09 16:53:20 -0300

    add button to login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,7 +9,7 @@ export const PageLogin = () => {
 
     return (
         <div className="w-screen grid grid-cols-2 items-center justify-center">
-            <aside className="bg-purple-500 h-screen flex flex-col justify-center px-4 gap-4">
+            <aside className="bg-purple-500 h-screen w-full flex flex-col justify-center px-4 gap-4">
                 <img src="/assets/hero.svg" alt="" className="w-screen h-3/6" />
                 <h1 className="font-bold text-white text-4xl">
                     Toda pergunta tem uma resposta.
@@ -18,10 +18,19 @@ export const PageLogin = () => {
                     Aprenda e compartilhe conhecimento com outras pessoas.
                 </h3>
             </aside>
-            <main>
-                <button onClick={onClick}>Crie sua sala com o Google</button>
-                <input type="text" placeholder="Digite o código da sala" />
-                <button>Entrar na sala</button>
+            <main className="bg-purple-50 h-screen w-full flex flex-col items-center justify-center px-4 gap-4">
+                <img src="/assets/logo.svg" alt="" className="w-32" />
+                <form className="flex flex-col w-3/4 gap-4">
+                    <button
+                        className="bg-red-500 px-8 py-4 text-white font-semibold text-base flex gap-4 items-center justify-center"
+                        onClick={onClick}
+                    >
+                        <img src="/assets/google.svg" alt="" />
+                        Crie sua sala com o Google
+                    </button>
+                    <input type="text" placeholder="Digite o código da sala" />
+                    <button>Entrar na sala</button>
+                </form>
             </main>
         </div>
     )
