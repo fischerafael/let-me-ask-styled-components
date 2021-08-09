cdade4569aeea90a8118983f684120bd40126b4a
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-09 16:46:29 -0300

    add hero section

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,10 +8,21 @@ export const PageLogin = () => {
     }
 
     return (
-        <div className="w-screen h-screen flex flex-col items-center justify-center">
-            <button onClick={onClick}>Crie sua sala com o Google</button>
-            <input type="text" placeholder="Digite o código da sala" />
-            <button>Entrar na sala</button>
+        <div className="w-screen grid grid-cols-2 items-center justify-center">
+            <aside className="bg-purple-500 h-screen flex flex-col justify-center px-4 gap-4">
+                <img src="/assets/hero.svg" alt="" className="w-screen h-3/6" />
+                <h1 className="font-bold text-white text-4xl">
+                    Toda pergunta tem uma resposta.
+                </h1>
+                <h3 className="text-white text-base">
+                    Aprenda e compartilhe conhecimento com outras pessoas.
+                </h3>
+            </aside>
+            <main>
+                <button onClick={onClick}>Crie sua sala com o Google</button>
+                <input type="text" placeholder="Digite o código da sala" />
+                <button>Entrar na sala</button>
+            </main>
         </div>
     )
 }
