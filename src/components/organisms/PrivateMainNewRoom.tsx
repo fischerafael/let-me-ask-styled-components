ca8aeb9d6bb00171a2ba5fa55f9bc8b82ac1f4da
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-10 16:44:07 -0300

    bind createRoom data

--- src/components/organisms/PrivateMainNewRoom.tsx
+++ src/components/organisms/PrivateMainNewRoom.tsx
@@ -1,12 +1,16 @@
+import { FormEvent } from 'react'
+import { database } from '../../services/firebase'
 import { handleNavigateTo } from '../../utils/handleNavigateTo'
 import { MainButton } from '../atoms/Button'
 
-export const PrivateMainNewRoom = () => {
+export const PrivateMainNewRoom = ({ onSubmit, onChange, value }) => {
     return (
         <main className="bg-purple-50 h-screen w-full flex flex-col items-center justify-center px-4 gap-4">
             <img src="/assets/logo.svg" alt="" className="w-32" />
-            <form className="flex flex-col w-3/4 gap-4">
+            <form onSubmit={onSubmit} className="flex flex-col w-3/4 gap-4">
                 <input
+                    value={value}
+                    onChange={onChange}
                     type="text"
                     placeholder="Nome da sala"
                     className="h-14 flex items-center justify-center px-8 border border-gray-200 rounded-lg"
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,34 @@
+import { FormEvent, useState } from 'react'
 import { PrivateMainNewRoom } from '../components/organisms/PrivateMainNewRoom'
 import { PublicSideBar } from '../components/organisms/PublicSideBar'
 import { TemplatePublic } from '../components/templates/TemplatePublic'
-import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 
 export const PageNewRoom = () => {
-    const { user } = useAuth()
+    const [formData, setFormData] = useState({ newRoom: '' })
+
+    console.log('formData', formData)
+
+    const handleCreateRoom = async (e: FormEvent) => {
+        e.preventDefault()
+        alert('ma oie')
+        try {
+            const roomReference = database.ref('rooms')
+        } catch (e) {}
+    }
 
     return (
         <TemplatePublic
             sidebar={<PublicSideBar />}
-            main={<PrivateMainNewRoom />}
+            main={
+                <PrivateMainNewRoom
+                    onSubmit={handleCreateRoom}
+                    value={formData.newRoom}
+                    onChange={(e) =>
+                        setFormData({ ...formData, newRoom: e.target.value })
+                    }
+                />
+            }
         />
     )
 }
