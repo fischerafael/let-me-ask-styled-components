544c5d76efa9c4f9b3f4c45936f006049d0f5cba
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-12 20:51:06 -0300

    add header

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,17 @@
 export const PageRoom = ({ roomId }: { roomId: string }) => {
     return (
-        <div>
-            <h1>{roomId}</h1>
+        <div className="bg-purple-50 w-screen h-screen flex flex-col items-center">
+            <header className="w-full border border-b h-1/6 flex justify-center">
+                <section className="max-w-screen-lg w-full flex justify-between items-center">
+                    <img src="/assets/logo.svg" alt="" className="w-28" />
+                    <section>
+                        <section></section>
+                        <section>
+                            <p>Sala #{roomId}</p>
+                        </section>
+                    </section>
+                </section>
+            </header>
         </div>
     )
 }
